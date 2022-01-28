042dd032287e6528a14f45d7639aca0a684f7e30
tushargoplani <tushar7140@gmail.com>
2022-01-28 12:16:11 +0530

    completed

---
+++ consumer2.js
@@ -0,0 +1,22 @@
+const amqp = require("amqplib");
+
+connect();
+
+async function connect() {
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        channel.consume("jobs", message =>{
+            const input = JSON.parse(message.content.toString());
+            console.log(`received job with input ${input.number}`);
+            if(input.number == 7){
+                channel.ack(message);
+            }
+        })
+        console.log("Waiting for messages ..");
+    }
+    catch (ex) {
+        console.log(ex);
+    }
+}
---
+++ publisher.js
@@ -0,0 +1,18 @@
+const amqp = require("amqplib");
+
+const message = {number: process.argv[2]};
+
+connect();
+
+async function connect(){
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        channel.sendToQueue("jobs",Buffer.from(JSON.stringify(message)));
+        console.log(`Job sent successfully ${message.number}`);
+    }
+    catch(ex){
+        console.log(ex);
+    }
+}
